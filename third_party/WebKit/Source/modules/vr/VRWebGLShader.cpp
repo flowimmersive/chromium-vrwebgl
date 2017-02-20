048dab6a3a4e41486bfc572d34c253b5fc67dbc6
ijamardo <ijamardo@ijamardo-glaptop2.roam.corp.google.com>
2017-02-20 11:40:54 -0800

    Added all the properties and method necessary to store the cache data for programs and shaders.

--- third_party/WebKit/Source/modules/vr/VRWebGLShader.cpp
+++ third_party/WebKit/Source/modules/vr/VRWebGLShader.cpp
@@ -2,4 +2,47 @@
 
 namespace blink {
 
+VRWebGLShader::VRWebGLShader(GLenum type): 
+	cached(false),
+	m_type(type), 
+	m_compileStatus(0), 
+	m_deleted(false)
+{
+}
+
+void VRWebGLShader::setCompileStatus(GLint compileStatus)
+{
+	m_compileStatus = compileStatus;
+}
+
+void VRWebGLShader::setInfoLog(const std::string& infoLog)
+{
+	m_infoLog = infoLog;
+}
+
+void VRWebGLShader::markAsDeleted()
+{
+	m_deleted = true;
+}
+
+GLenum VRWebGLShader::getType() const
+{
+	return m_type;
+}
+
+GLint VRWebGLShader::getCompileStatus() const
+{
+	return m_compileStatus;
+}
+
+std::string VRWebGLShader::getInfoLog() const
+{
+	return m_infoLog;
+}
+
+bool VRWebGLShader::isDeleted() const
+{
+	return m_deleted;
+}
+
 } // namespace blink
--- third_party/WebKit/Source/modules/vr/VRWebGLShader.h
+++ third_party/WebKit/Source/modules/vr/VRWebGLShader.h
@@ -4,10 +4,31 @@
 #include "modules/vr/VRWebGLObject.h"
 #include "wtf/text/WTFString.h"
 
+#include <atomic>
+
 namespace blink {
 
 class VRWebGLShader final : public VRWebGLObject {
     DEFINE_WRAPPERTYPEINFO();
+
+	public:
+    VRWebGLShader(GLenum type);
+
+    void setCompileStatus(GLint compileStatus);
+    void setInfoLog(const std::string& infoLog);
+    void markAsDeleted();
+    GLenum getType() const;
+    GLint getCompileStatus() const;
+    std::string getInfoLog() const;
+    bool isDeleted() const;
+
+    std::atomic<bool> cached;
+
+  private:
+  	GLenum m_type;
+  	GLint m_compileStatus;
+  	std::string m_infoLog;
+  	bool m_deleted;
 };
 
 } // namespace blink
