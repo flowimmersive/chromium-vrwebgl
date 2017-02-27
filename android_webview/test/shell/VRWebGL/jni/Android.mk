b311810876f41dbdabdb003661017e2ff2831fdc
ijamardo <ijamardo@ijamardo-glaptop2.roam.corp.google.com>
2017-02-26 19:36:05 -0800

    As the Chromium version 57 already includes the GoogleVR SDK in the dependencies, use it to build the VRWebGL_GVRMobileSDK library.

--- android_webview/test/shell/VRWebGL/jni/Android.mk
+++ android_webview/test/shell/VRWebGL/jni/Android.mk
@@ -3,12 +3,12 @@ MY_LOCAL_PATH := $(call my-dir)
 # ==========================================================
 # gvr
 # ==========================================================
-LOCAL_PATH := ./3rdparty/gvr_sdk_mobile_1.20.0/armeabi-v7a
+LOCAL_PATH := ../../../../../third_party/gvr-android-sdk
 include $(CLEAR_VARS)
 LOCAL_MODULE := gvr
 LOCAL_SRC_FILES := \
-	libgvr.so
-include $(PREBUILT_SHARED_LIBRARY)
+	libgvr_shim_static_arm.a
+include $(PREBUILT_STATIC_LIBRARY)
 
 # ==========================================================
 # VRWebGL_GVRMobileSDK
@@ -18,7 +18,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := VRWebGL_GVRMobileSDK
 LOCAL_C_INCLUDES := \
 	. \
-	./3rdparty/gvr_sdk_mobile_1.20.0/include \
+	../../../../../third_party/gvr-android-sdk/src/libraries/headers \
 	../../../../../third_party/WebKit/Source
 LOCAL_SRC_FILES := \
 	./VRWebGL_GVRMobileSDK.cpp \
@@ -27,7 +27,7 @@ LOCAL_SRC_FILES := \
 	../../../../../third_party/WebKit/Source/modules/vr/VRWebGLMath.cpp \
 	../../../../../third_party/WebKit/Source/modules/vr/VRWebGL_gl.cpp 	
 LOCAL_CFLAGS := -std=gnu++11 -Werror 
-LOCAL_SHARED_LIBRARIES := \
+LOCAL_STATIC_LIBRARIES := \
 	gvr
 LOCAL_LDLIBS := -llog -landroid -lGLESv3 -lEGL
 include $(BUILD_SHARED_LIBRARY)
