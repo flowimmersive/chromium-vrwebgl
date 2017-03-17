695c322ac33d11c077ffe2299926d9ee14083d73
ijamardo <ijamardo@ijamardo-glaptop2.roam.corp.google.com>
2017-03-17 07:24:30 -0700

    The new VRWebGLWebView type that allows to handle a webview from javascript.

---
+++ third_party/WebKit/Source/modules/vr/VRWebGLWebView.cpp
@@ -0,0 +1,90 @@
+#include "modules/vr/VRWebGLWebView.h"
+#include "modules/vr/VRWebGLCommandProcessor.h"
+
+namespace blink {
+
+VRWebGLWebView* VRWebGLWebView::create()
+{
+	return new VRWebGLWebView();
+}
+
+VRWebGLWebView::~VRWebGLWebView()
+{
+	VRWebGLCommandProcessor::getInstance()->queueVRWebGLCommandForProcessing(VRWebGLCommand_deleteWebView::newInstance(m_textureId));
+}
+
+GLuint VRWebGLWebView::textureId() const
+{
+	return m_textureId;
+}
+
+String VRWebGLWebView::src() const
+{
+	return m_src;
+}
+
+void VRWebGLWebView::setSrc(const String& src)
+{
+	if (m_src != src)
+	{
+		VRWebGLCommandProcessor::getInstance()->queueVRWebGLCommandForProcessing(VRWebGLCommand_setWebViewSrc::newInstance(m_textureId, src.utf8().data()));
+	}
+	m_src = src;
+	m_loaded = false;
+}
+
+long VRWebGLWebView::width() const
+{
+	return VRWebGLCommandProcessor::getInstance()->getVideoWidth(m_textureId);
+}
+
+long VRWebGLWebView::height() const
+{
+	return VRWebGLCommandProcessor::getInstance()->getVideoHeight(m_textureId);
+}
+
+bool VRWebGLWebView::checkLoaded()
+{
+	return true;
+}
+
+void VRWebGLWebView::touchstart(float x, float y)
+{
+	VRWebGLCommandProcessor::getInstance()->queueVRWebGLCommandForProcessing(VRWebGLCommand_dispatchWebViewTouchEvent::newInstance(m_textureId, VRWebGLCommand_dispatchWebViewTouchEvent::TOUCH_START, x, y));
+}
+
+void VRWebGLWebView::touchmove(float x, float y)
+{
+	VRWebGLCommandProcessor::getInstance()->queueVRWebGLCommandForProcessing(VRWebGLCommand_dispatchWebViewTouchEvent::newInstance(m_textureId, VRWebGLCommand_dispatchWebViewTouchEvent::TOUCH_MOVE, x, y));
+}
+
+void VRWebGLWebView::touchend(float x, float y)
+{
+	VRWebGLCommandProcessor::getInstance()->queueVRWebGLCommandForProcessing(VRWebGLCommand_dispatchWebViewTouchEvent::newInstance(m_textureId, VRWebGLCommand_dispatchWebViewTouchEvent::TOUCH_END, x, y));
+}
+
+void VRWebGLWebView::back()
+{
+	VRWebGLCommandProcessor::getInstance()->queueVRWebGLCommandForProcessing(VRWebGLCommand_dispatchWebViewNavigationEvent::newInstance(m_textureId, VRWebGLCommand_dispatchWebViewNavigationEvent::NAVIGATION_BACK));
+}
+
+void VRWebGLWebView::forward()
+{
+	VRWebGLCommandProcessor::getInstance()->queueVRWebGLCommandForProcessing(VRWebGLCommand_dispatchWebViewNavigationEvent::newInstance(m_textureId, VRWebGLCommand_dispatchWebViewNavigationEvent::NAVIGATION_FORWARD));
+}
+
+void VRWebGLWebView::reload()
+{
+	VRWebGLCommandProcessor::getInstance()->queueVRWebGLCommandForProcessing(VRWebGLCommand_dispatchWebViewNavigationEvent::newInstance(m_textureId, VRWebGLCommand_dispatchWebViewNavigationEvent::NAVIGATION_RELOAD));
+}
+
+VRWebGLWebView::VRWebGLWebView()
+{
+	m_textureId = *(GLuint*)VRWebGLCommandProcessor::getInstance()->queueVRWebGLCommandForProcessing(VRWebGLCommand_newWebView::newInstance());
+}
+
+DEFINE_TRACE(VRWebGLWebView)
+{
+}
+
+} // namespace blink
---
+++ third_party/WebKit/Source/modules/vr/VRWebGLWebView.h
@@ -0,0 +1,49 @@
+#ifndef VRWebGLWebView_h
+#define VRWebGLWebView_h
+
+#include "bindings/core/v8/ScriptWrappable.h"
+#include "wtf/text/WTFString.h"
+
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <GLES3/gl3.h>
+
+namespace blink {
+
+class VRWebGLWebView final : public GarbageCollectedFinalized<VRWebGLWebView>, public ScriptWrappable {
+    DEFINE_WRAPPERTYPEINFO();
+public:
+    static VRWebGLWebView* create();
+    ~VRWebGLWebView();
+
+    GLuint textureId() const;
+
+    String src() const;
+    void setSrc(const String&);
+
+    long width() const;
+    long height() const;
+
+    bool checkLoaded();
+
+    void touchstart(float x, float y);
+    void touchmove(float x, float y);
+    void touchend(float x, float y);
+
+    void back();
+    void forward();
+    void reload();
+
+    DECLARE_TRACE()
+
+private:
+    VRWebGLWebView();
+
+    GLuint m_textureId = -1;
+    String m_src;
+    bool m_loaded = false;
+};
+
+} // namespace blink
+
+#endif // VRWebGLWebView_h
